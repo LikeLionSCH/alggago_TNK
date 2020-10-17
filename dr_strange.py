8c99577ac5c386022c6f276fca08cec6061002be
HwangSB <hsb990105@gmail.com>
2020-10-18 02:06:16 +0900

    코드 정리

--- dr_strange.py
+++ dr_strange.py
@@ -153,38 +153,6 @@ def predict(positions, max_layer_count, my_turn=True, prefix=''):
     # 최고점을 받은 경우를 반환
     return current_best_case
 
+
+# 사용 예시
 best_case = predict(positions, 3)
-
-best_case.score
-best_case.strength
-
-# GAME_STATE_UNKNOWN = 0
-# GAME_STATE_WIN = 1
-# GAME_STATE_LOSE = 2
-# GAME_STATE_PLAYING = 3
-# GAME_STATE_DRAW = 4
-
-# def get_game_state(positions):
-#     # 남아있는 상대 돌이 없고, 우리 돌이 한개 이상 있으면 승리
-#     if len(positions[1]) <= 0 and len(positions[0]) > 0:
-#         return GAME_STATE_WIN
-
-#     # 남아있는 우리 돌이 없고, 상대 돌이 한개 이상 있으면 패배
-#     if len(positions[0]) <= 0 and len(positions[1]) > 0:
-#         return GAME_STATE_LOSE
-
-#     # 둘다 돌이 없으면 무승부
-#     if len(positions[0]) <= 0 and len(positions[1]) <= 0:
-#         return GAME_STATE_DRAW
-
-#     # 그 외의 경우에는 무승부
-#     return GAME_STATE_PLAYING
-
-
-# def predict(positions):
-#     if get_game_state(positions) == GAME_STATE_WIN:
-#         return 10
-#     elif get_game_state(positions) == GAME_STATE_LOSE:
-#         return 0
-#     elif get_game_state(positions) == GAME_STATE_DRAW:
-#         return 
