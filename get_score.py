21f9b322c2557e205800a7f0c61e1c214ca5b279
KimTaewan <wwan13@naver.com>
2020-10-17 21:15:50 +0900

    update get_priority function parameters

--- get_score.py
+++ get_score.py
@@ -27,18 +27,20 @@ def get_score(my_stone_num,opp_stone_num,my_stone_near,opp_stone_near):
 
     return total_score #최종 점수 반환
 
-def get_priority(score_list):
+def get_priority(score_list,max_or_min,num_of_case):
     """ 가장 잘 친 경우 3가지를 반환하는 함수 """
     # score_list -> 점수 정보 모두를 갖고 있는 리스트 
+    # max_or_min -> 최대값으로 찾으려면 1, 최소값으로 찾으려면 -1
+    # num_of_case -> 반환받을 개수 입력
 
     heap= [] # 힙 생성
     for score in score_list:
-        heapq.heappush(heap,int(score)*-1) # 받아온 점수 리스트 힙에 push
+        heapq.heappush(heap,int(score)*(-1*int(max_or_min))) # 받아온 점수 리스트 힙에 push
         # 내림차순으로 정렬 해야 하기 때문에 -1을 곱하여 푸쉬 한다.   
-        #print(heap)
+        print(heap)
 
     list = []
-    for i in range(0,16): # 3개만 뽑는다
+    for i in range(0,int(num_of_case)): #
         list.append(-1*heapq.heappop(heap)) # 제일 큰값을 차례대로 뽑는데 
 
     return list # 큰값 3개 뽑은거 return
@@ -53,9 +55,9 @@ stone_infos = file.readlines()
 score_info = []
 for stone_info in stone_infos:
     tmp = stone_info.split(' ') # ' '를 기준으로 나눔
-    get_score(int(tmp[0]),int(tmp[1])) # 우리돌 상대돌  집어 넣어서 점수 계산
-    score_info.append(get_score(int(tmp[0]),int(tmp[1]))) # 리스트에 추가
-priority_list = get_priority(score_info) # 리스트 보내서 3개 불러옴
+    #get_score(int(tmp[0]),int(tmp[1]),int(tmp[2]),int(tmp[3])) # 우리돌 상대돌  집어 넣어서 점수 계산
+    score_info.append(get_score(int(tmp[0]),int(tmp[1]),int(tmp[2]),int(tmp[3]))) # 리스트에 추가
+priority_list = get_priority(score_info,1,5) 
 
-print("\n"+"최대값 3개")
+print("\n"+"최대값 16개")
 print(priority_list)
